d3ee60bc41931ca57aafa4a1349d9c28308f532b
Kuti Gbolahan <kutigbolahan.ab@gmail.com>
2022-07-26 12:43:13 +0100

    Initial Commit

---
+++ Scrumlist/ScrumlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumlistApp.swift
+//  Scrumlist
+//
+//  Created by GB on 7/26/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
